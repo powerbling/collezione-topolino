731bf4931eebeada43c0c970f9a0eaa85880afb2
powerbling <zambon.pietro0@gmail.com>
2022-10-28 11:50:44 +0200

    Fix Pointer Events Catching
    The number overlay in the custom scroller catches the events even when
    its opacity is 0. Added IgnorePointer to the overlay to fix this issue.

--- lib/components/custom_scroller.dart
+++ lib/components/custom_scroller.dart
@@ -77,25 +77,27 @@ class _CustomScrollerState extends State<CustomScroller> {
           controller: widget._scrollController,
           child: widget.child,
         ),
-        Center(
-          child: AnimatedOpacity(
-            opacity: _overlayVisible ? 0.8 : 0.0,
-            duration: const Duration(milliseconds: 200),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20.0),
-                color: Colors.grey[400],
-              ),
-              child: SizedBox.square(
-                dimension: 150.0,
-                child: Center(
-                    child: Text(
-                  _currentElement.toString(),
-                  style: Theme.of(context)
-                      .textTheme
-                      .displaySmall
-                      ?.copyWith(color: Colors.white),
-                )),
+        IgnorePointer(
+          child: Center(
+            child: AnimatedOpacity(
+              opacity: _overlayVisible ? 0.8 : 0.0,
+              duration: const Duration(milliseconds: 200),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Colors.grey[400],
+                ),
+                child: SizedBox.square(
+                  dimension: 150.0,
+                  child: Center(
+                      child: Text(
+                    _currentElement.toString(),
+                    style: Theme.of(context)
+                        .textTheme
+                        .displaySmall
+                        ?.copyWith(color: Colors.white),
+                  )),
+                ),
               ),
             ),
           ),
